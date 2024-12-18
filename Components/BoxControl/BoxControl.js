ce650ea5c874f0451c39f81d4c59a4ecce30735a
wasek23 <wasekbellah2311@gmail.com>
2024-12-18 14:32:58 +0600

    icons

--- Components/BoxControl/BoxControl.js
+++ Components/BoxControl/BoxControl.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { __experimentalUnitControl as UnitControl } from '@wordpress/components';
 
 import './BoxControl.scss';
