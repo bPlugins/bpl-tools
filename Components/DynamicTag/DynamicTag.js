4378f67d648adf25cafccef1163553e0591019c8
Murad Wahid <muradwahid334@gmail.com>
2025-02-09 12:43:01 +0600

    added two new components tinyeditor & dynamictag

---
+++ Components/DynamicTag/DynamicTag.js
@@ -0,0 +1,13 @@
+
+/**
+ * DynamicTag component
+ * @param {string} tagName - The tag name of the component
+ * @param {Object} props - The props of the component
+ * @returns {ReactElement} - The dynamic tag component
+ */
+
+const DynamicTag = ({ tagName: DynamicTag, ...props }) => {
+  return <DynamicTag {...props}></DynamicTag>;
+};
+
+export default DynamicTag;
