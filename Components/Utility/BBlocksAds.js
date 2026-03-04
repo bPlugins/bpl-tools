7a84352c6a6ff3120c001de0a3ff7f27a245322f
wasek23 <wasekbellah2311@gmail.com>
2026-03-04 10:12:16 +0600

    fix bblocks name

--- Components/Utility/BBlocksAds.js
+++ Components/Utility/BBlocksAds.js
@@ -1,4 +1,4 @@
 const BBlocksAds = () => {
-	return <span>Need more block like this? Checkout the bundle ➡ <a href='https://wordpress.org/plugins/b-blocks' target='_blank' rel='noopener noreferrer'>B Blocks</a></span>;
+	return <span>Need more block like this? Checkout the bundle ➡ <a href='https://wordpress.org/plugins/b-blocks' target='_blank' rel='noopener noreferrer'>bBlocks</a></span>;
 }
 export default BBlocksAds;
