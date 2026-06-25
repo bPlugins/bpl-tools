2c56ebce301ca15f6d515b259383acf500241ad0
wasek23 <wasekbellah2311@gmail.com>
2026-06-25 18:11:17 +0600

    update border control

--- Components/Deprecated/BorderControl/BorderControl.js
+++ Components/Deprecated/BorderControl/BorderControl.js
@@ -9,6 +9,7 @@
 
 import { __ } from '@wordpress/i18n';
 import { Dropdown, PanelRow, SelectControl, __experimentalUnitControl as UnitControl, Button } from '@wordpress/components';
+import { useState } from 'react';
 
 import Label from '../../Label/Label';
 import { ColorControl } from '../../ColorControl/ColorControl';
@@ -16,8 +17,21 @@ import { borderStyles, pxUnit, perUnit, emUnit, remUnit, sides } from '../../../
 
 const BorderControl = props => {
 	const { className = '', label = __('Border:'), value, onChange, defaults = {} } = props;
+	const [radiusLinked, setRadiusLinked] = useState(true);
 
-	const defaultVal = { width: '0px', style: 'solid', color: '', side: 'all', radius: '0px' }
+	const defaultVal = { width: '0px', style: 'solid', color: '', side: 'all', radius: '0px 0px 0px 0px' };
+
+	const parseRadius = (radiusVal) => {
+		if (!radiusVal) return ['0px', '0px', '0px', '0px'];
+		const parts = radiusVal.trim().split(/\s+/);
+		if (parts.length === 4) return parts;
+		if (parts.length === 1) return [parts[0], parts[0], parts[0], parts[0]];
+		return ['0px', '0px', '0px', '0px'];
+	};
+
+	const joinRadius = (radiusArray) => {
+		return radiusArray.join(' ');
+	};
 
 	const getDefault = property => defaults?.[property] || defaultVal[property];
 	const setDefault = property => onChange({ ...value, [property]: getDefault(property) });
@@ -25,6 +39,29 @@ const BorderControl = props => {
 	const getValue = property => value?.[property] || getDefault(property);
 	const setValue = (property, val) => onChange({ ...value, [property]: val });
 	const resetValue = property => <Button icon='image-rotate' className='bPlResetVal' onClick={() => setDefault(property)} />
+
+	const handleRadiusChange = (val, corner) => {
+		const currentRadius = parseRadius(getValue('radius'));
+		const cornerIndex = { 'topLeft': 0, 'topRight': 1, 'bottomRight': 2, 'bottomLeft': 3 };
+
+		if (radiusLinked) {
+			setValue('radius', joinRadius([val, val, val, val]));
+		} else {
+			currentRadius[cornerIndex[corner]] = val;
+			setValue('radius', joinRadius(currentRadius));
+		}
+	};
+
+	const radiusValues = parseRadius(getValue('radius'));
+	const defaultRadiusValues = parseRadius(getDefault('radius'));
+	const isRadiusReset = radiusValues.every((val, i) => val === defaultRadiusValues[i]);
+
+	const radiusCorners = [
+		{ key: 'topLeft', index: 0, label: __('Top-left') },
+		{ key: 'topRight', index: 1, label: __('Top-right') },
+		{ key: 'bottomRight', index: 2, label: __('Bottom-right') },
+		{ key: 'bottomLeft', index: 3, label: __('Bottom-left') },
+	];
 
 	return <PanelRow className={`bPlDropdown ${className}`}>
 		<Label className='mt5'>{label}</Label>
@@ -51,10 +88,29 @@ const BorderControl = props => {
 					{value?.side && value?.side !== getDefault('side') && resetValue('side')}
 				</PanelRow>
 
-				<PanelRow>
-					<UnitControl label={__('Radius:')} labelPosition='left' value={getValue('radius')} onChange={val => setValue('radius', val)} units={[pxUnit(50), perUnit(50), emUnit(3), remUnit(3)]} isResetValueOnUnitChange={true} />
-					{value?.radius && value?.radius !== getDefault('radius') && resetValue('radius')}
-				</PanelRow>
+				<div className='bPlBoxControl mt20'>
+					<Label className=''>{__('Radius:')}</Label>
+
+					<div className='sides'>
+						{radiusCorners.map(corner => <div className='bplUnitControlWrapper' key={corner.key}>
+							<UnitControl
+								onChange={(v) => handleRadiusChange(v, corner.key)}
+								value={radiusValues[corner.index]}
+								units={[pxUnit(50), perUnit(50), emUnit(3), remUnit(3)]}
+								isResetValueOnUnitChange={true}
+							/>
+							{/* <div className='sideLabel'>{corner.label}</div> */}
+						</div>)}
+
+						<button className={`bplBoxControlLinkButton ${radiusLinked ? 'activeLink' : ''}`} onClick={() => setRadiusLinked(!radiusLinked)}>
+							{radiusLinked ? <span className='dashicons dashicons-admin-links'></span> : <span className='dashicons dashicons-editor-unlink'></span>}
+						</button>
+
+						{!isRadiusReset && <button className='bplBoxControlLinkButton' onClick={() => setDefault('radius')}>
+							<span className='dashicons dashicons-image-rotate'></span>
+						</button>}
+					</div>
+				</div>
 			</>}
 		/>
 	</PanelRow>
